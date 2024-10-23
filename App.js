e60bf5d1b597e498728c7a779a234cf1d3817921
Yee Mon Thu <23008873@myrp.edu.sg>
2024-10-23 14:18:08 +0800

    C346 L2movie

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import React from 'react';
+import {View, Text, StatusBar, Image, ScrollView} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome6";
+
+
+const Movie = ({title, year, icon_name, poster}) => {
+    return (
+        <ScrollView>
+            <Image source={poster} style={{width:400, height: 400}}/>
+            <Icon name = {icon_name} size={20}><Text>{title} {year}</Text></Icon>
+        </ScrollView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+      <ScrollView>
+          <StatusBar hidden={true}/>
+          <Movie title= "Doctor Sleep" year = "2019" icon_name="skull" poster={require("./img/doctor-sleep.jpg")}/>
+          <Movie title= "Midway" year = "2020" icon_name="person-rifle" poster={require("./img/midway.jpg")}/>
+      </ScrollView>
+  );
+}
+export default App;
+
